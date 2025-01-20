e89e1aa837b92d0d72be711d834f600aafcf6a51
neochaos42 <125705606+neochaos42@users.noreply.github.com>
2025-01-19 21:13:57 -0600

    Update AdBanner.tsx

--- apps/desktop/packages/mainWindow/src/components/AdBanner.tsx
+++ apps/desktop/packages/mainWindow/src/components/AdBanner.tsx
@@ -2,13 +2,6 @@ import adSize from "@/utils/adhelper"
 
 export const AdsBanner = () => {
   return (
-    <div
-      style={{
-        height: `${adSize.height}px`,
-        width: `${adSize.width}px`
-      }}
-    >
-      <owadview class="relative z-100" />
-    </div>
+    <div></div>
   )
 }
